db374d3f8af9e99fa9255e3ea712f4132bd1fb2e
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-27 11:36:40 -0700

    newcommit

--- lando/Program.cs
+++ lando/Program.cs
@@ -7,4 +7,5 @@ Console.WriteLine($"Can i tell you a story?");
 var Responsename = Console. ReadLine();
 
 Console.WriteLine($"There was once a cat that lived on the moon, what should his name be {personName}");
-int myAge = 33 ;
+int myAge = 33; Console. ReadLine(); 
+
