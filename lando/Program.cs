58277a47977d768ad7be165512f4f2c6f3c6213a
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-27 10:27:20 -0700

    initial commit

---
+++ lando/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Everyone!");
+var personName =  Console. ReadLine();
+
+Console.WriteLine($"hi {personName}, my name is Robot");
+Console.WriteLine($"Can i tell you a story?");
+var Responsename = Console. ReadLine();
+
+Console.WriteLine($"There was once a cat that lived on the moon, what should his name be {personName}");
